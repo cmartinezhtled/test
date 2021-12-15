42086af98596608051ce4e4b049a8b97eb3f3c15
cmartinezhtled <cmartinez@scholem.edu.ar>
2021-12-15 14:49:58 -0300

    agregué notificaciones de prueba

--- script.js
+++ script.js
@@ -4,15 +4,44 @@ if ('serviceWorker' in navigator ) {
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
 
-  Notification.requestPermission(function(status) {
-    console.log('Notification permission status:', status);
+  
   });
 }
+
+if ('Notification' in window && navigator.serviceWorker) {
+  // Display the UI to let the user toggle notifications
+  Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+}
+
+
 
 function displayNotification() {
   if (Notification.permission == 'granted') {
     navigator.serviceWorker.getRegistration().then(function(reg) {
-      reg.showNotification('Hello world!');
+      var options = {
+        body: 'Here is a notification body!',
+        icon: './img/icon_64.png',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        },
+        actions: [
+          {action: 'explore', title: 'Explore this new world',
+            icon: 'images/checkmark.png'},
+          {action: 'close', title: 'Close notification',
+            icon: './img/icon_32.png'},
+        ]
+      };
+      reg.showNotification('Hello world!', options);
     });
   }
+  else if (Notification.permission === "blocked") {
+    /* the user has previously denied push. Can't reprompt. */
+   } else {
+     /* show a prompt to the user */
+   }
+
+
 }
--- sw.js
+++ sw.js
@@ -63,3 +63,22 @@ self.addEventListener('fetch', e => {
   )
 })
 
+self.addEventListener('notificationclick', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+  var action = e.action;
+
+  if (action === 'close') {
+    notification.close();
+  } else {
+    clients.openWindow('http://www.scholem.edu.ar');
+    notification.close();
+  }
+});
+
+self.addEventListener('notificationclose', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+
+  console.log('Closed notification: ' + primaryKey);
+});
