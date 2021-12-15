71f1398b9dc7ce1817e991418c02c0f1b17e2c35
cmartinezhtled <75027361+cmartinezhtled@users.noreply.github.com>
2021-12-15 08:06:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  aa => console.log( 'Service Worker y  Push son soportados', aa);
+  
   navigator.serviceWorker.register('./sw.js')
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
+}
+else {
+  console.warn('Push notifications no soportado');
+  pushButton.textContent = 'Push No Soportado';
 }
