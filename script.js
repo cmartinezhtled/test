13a6acec74bbaaea13db90b6510fa6250480d980
cmartinezhtled <cmartinez@scholem.edu.ar>
2021-12-15 13:56:23 -0300

    volví todo atrás

--- script.js
+++ script.js
@@ -4,13 +4,7 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
 
-  navigator.serviceWorker.getRegistration().then(reg => {
-    reg.pushManager.subscribe({
-      userVisibleOnly: true
-    }).then(sub => {
-      // send sub.toJON() to server
-    });
-  });
+  
 }
 else {
   console.warn('Push notifications no soportado');
