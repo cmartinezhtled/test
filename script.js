d55f0d47996904d1898cb187572b66c5ee0eeea3
cmartinezhtled <cmartinez@scholem.edu.ar>
2021-12-15 12:44:42 -0300

    agregué registración de push notif

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
   navigator.serviceWorker.register('./sw.js')
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
+
+  navigator.serviceWorker.getRegistration().then(reg => {
+    reg.pushManager.subscribe({
+      userVisibleOnly: true
+    }).then(sub => {
+      // send sub.toJON() to server
+    });
+  });
 }
 else {
   console.warn('Push notifications no soportado');
