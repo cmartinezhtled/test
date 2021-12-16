e341702ea611d9e4251dc3ef8ef8b292f62c7fcf
cmartinezhtled <cmartinez@scholem.edu.ar>
2021-12-16 11:39:30 -0300

    agregué notificaciones de prueba

--- scripts/main.js
+++ scripts/main.js
@@ -44,57 +44,7 @@ navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
     })
     .catch(function(err) {
       window.Demo.debug.log('Error during getSubscription()', err);
-    });
-
-}
-
-
-
-function initialiseUI() {
-  console.log('initialiseUI...............');
-
-  pushButton.addEventListener('click', function() {
-    pushButton.disabled = true;
-    if (isSubscribed) {
-      // TODO: Unsubscribe user
-    } else {
-      subscribeUser();
-    }
+    })
+    
   });
-
-  // Set the initial subscription value
-  swRegistration.pushManager.getSubscription()
-  .then(function(subscription) {
-    console.log('subscription:'+subscription);
-
-    isSubscribed = !(subscription === null);
-
-    if (isSubscribed) {
-      console.log('User IS subscribed.');
-    } else {
-      console.log('User is NOT subscribed.');
-    }
-
-    updateBtn();
-  });
-  
-}
-
-const applicationServerPublicKey = 'BLDUWLUIaRb8OiTV7PqAsnwopoYUbiUTP3zF-9tbxW7MwxisVK0LoZbKYD11btoDfeKaOLQ1KC5Ot_v4XBMBwjc';
-
-
-function updateBtn() {
-  if (isSubscribed) {
-    pushButton.textContent = '1Disable Push Messaging';
-  } else {
-    pushButton.textContent = '1Enable Push Messaging';
-  }
-
-  pushButton.disabled = false;
-}
-
-
-
-
-
 
