87108a37c69b0748db935aa87bd6ce95659eea77
cmartinezhtled <cmartinez@scholem.edu.ar>
2021-12-16 09:27:41 -0300

    agregué notificaciones de prueba

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
+function initialiseUI() {
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    updateBtn();
+  });
+}
+
+
+function updateBtn() {
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+
+  pushButton.disabled = false;
+}
+
+
+
+
 if ('serviceWorker' in navigator) {
   
   navigator.serviceWorker.register('./sw.js')
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
 
- 
+    swRegistration = swReg;
+    initialiseUI();
 }
-
-
-
-
 
 
 
