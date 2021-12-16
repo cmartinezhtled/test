adec5c390a67ac8a9b5636de483094195652cc58
cmartinezhtled <cmartinez@scholem.edu.ar>
2021-12-16 09:18:30 -0300

    agregué notificaciones de prueba

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-if ('serviceWorker' in navigator ) {
+if ('serviceWorker' in navigator) {
   
   navigator.serviceWorker.register('./sw.js')
-    .then(reg => console.log('Registro de SW exitoso', reg)
+    .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
+
+ 
 }
 
-if ('Notification' in window ) {
-  // Display the UI to let the user toggle notifications
-  Notification.requestPermission().then(function(status) {
-    console.log('Notification permission status:', status);
-  });
-}
+
+
+
+
 
 
 
