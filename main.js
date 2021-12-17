d6ad4bdb440d6bb1d519cb03380c2c0182f305dd
cmartinezhtled <cmartinez@scholem.edu.ar>
2021-12-17 11:05:45 -0300

    agregué notificaciones de prueba

--- main.js
+++ main.js
@@ -29,54 +29,6 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
 function initialiseUI () {
   console.log('ENTRO A SUSCRIPCION--->');
 
-  const applicationServerKey = 'BLDUWLUIaRb8OiTV7PqAsnwopoYUbiUTP3zF-9tbxW7MwxisVK0LoZbKYD11btoDfeKaOLQ1KC5Ot_v4XBMBwjc';
-  swRegistration
-    .pushManager
-    .getSubscription()
-    .then(subscription => {
-      const isSubscribed = !(subscription === null);
-      if (!isSubscribed) {
-        return swRegistration.pushManager
-          .subscribe({
-            userVisibleOnly: true,
-            applicationServerKey,
-          })
-          .then(sendSubscriptionToServer);
-      }
-      sendSubscriptionToServer(subscription);
-    })
-    .catch(err => {
-      console.log('getSubscription failed', err);
-    });
-
-
-
-
-
-
-
-    const webpush = require('web-push');
-
-  const vapidKeys = {
-    publicKey: 'BLDUWLUIaRb8OiTV7PqAsnwopoYUbiUTP3zF-9tbxW7MwxisVK0LoZbKYD11btoDfeKaOLQ1KC5Ot_v4XBMBwjc',
-    privateKey: 'dkAqBXBpVSzbmXnPpGqT5rIGMeN7-qHdLiz_CNLK2l4',
-  };
-
-  webpush.setVapidDetails(
-    'mailto:cmartinez@scholem.edu.ar',
-    vapidKeys.publicKey,
-    vapidKeys.privateKey
-  );
-
-  webpush.sendNotification(
-    JSON.parse(subscription),
-    JSON.stringify({
-      title: 'Title',
-      icon: 'push-icon.png',
-      body: 'Body',
-      url: 'https://your-site.com/url-to-open',
-    })
-  )
 
 }
 
