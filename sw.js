640fdcaa65aac53107366103afd012dc8fdf10a9
cmartinezhtled <cmartinez@scholem.edu.ar>
2021-12-17 11:25:56 -0300

    agregué notificaciones de prueba

--- sw.js
+++ sw.js
@@ -65,7 +65,7 @@ self.addEventListener('fetch', e => {
 
 
 self.addEventListener('push', function(event) {
-  console.log(event.data.text);
+  console.log('EVENT:' + event.data.text);
 
   const message = JSON.parse(event.data.text());
   const title = message.title;
