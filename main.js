274f52c1e33b4f8aea9fc53808740ba48c0e53bb
cmartinezhtled <cmartinez@scholem.edu.ar>
2021-12-17 14:49:27 -0300

    agregué notificaciones de prueba

--- main.js
+++ main.js
@@ -68,6 +68,7 @@ function initialiseUI () {
 
 
 function notifyMe() {
+  
   console.log('LO QUE LEE Notification.requestPermission():' + Notification.requestPermission);
 
   // Comprobemos si el navegador admite notificaciones
