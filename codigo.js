cc8118e9c074b4716e24dffdab234e126554aece
cmartinezhtled <cmartinez@scholem.edu.ar>
2021-12-20 13:48:43 -0300

    agregado

--- codigo.js
+++ codigo.js
@@ -1,6 +1,14 @@
 //var dias = ["lunes","martes","miercoles","jueves","viernes"]    //array
 //console.log(dias[1]);
 
-let contacto = {"nombre":"Julieta", "email":"juli@gmail.com", "edad":21}
-console.log(contacto.nombre);
+//let contacto = {"nombre":"Julieta", "email":"juli@gmail.com", "edad":21}
+//console.log(contacto.nombre);
+
+//var colors = require('colors');
+//let contacto = {"nombre":"Julieta", "email":"juli@gmail.com", "edad":21}
+//console.log(contacto.email.green);
+
+var prueba = require('./funciones/funcion1');
+ prueba.suma(10,5);
+ prueba.resta(10,5);
 
