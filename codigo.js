9ab1c51450f236679595457412c9c06f5edd54c8
cmartinezhtled <cmartinez@scholem.edu.ar>
2021-12-20 12:43:04 -0300

    agregado

--- codigo.js
+++ codigo.js
@@ -1,1 +1,6 @@
-console.log('prueba');
+//var dias = ["lunes","martes","miercoles","jueves","viernes"]    //array
+//console.log(dias[1]);
+
+let contacto = {"nombre":"Julieta", "email":"juli@gmail.com", "edad":21}
+console.log(contacto.nombre);
+
